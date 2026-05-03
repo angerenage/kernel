46f5e99ff8c5b24f1664876f6c03131dbaf0cd28
Angerenage <ange.renage@gmail.com>
2026-05-03 07:00:08 +0200

    Fix strdup implementation to use strndup

--- base/libk/string/strdup.c
+++ base/libk/string/strdup.c
@@ -1,14 +1,5 @@
-#include <core/kheap.h>
-#include <string.h>
+#include <libk/string.h>
 
 char* strdup(const char* str) {
-	size_t length;
-	char*  copy;
-
-	length = strlen(str) + 1u;
-	copy   = kmalloc(length);
-	if (copy == NULL) return NULL;
-
-	memcpy(copy, str, length);
-	return copy;
+	return strndup(str, strlen(str));
 }
